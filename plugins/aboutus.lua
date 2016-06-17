1cdb2044aeb72c816941171750ae9d3c11bacc06
Amirho3inf <beatbotgroup@gmail.com>
2016-06-18 04:00:49 +0430

    Add files via upload

---
+++ plugins/aboutus.lua
@@ -0,0 +1,116 @@
+﻿local function do_keyboard_aboutus()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+    		{text = 'Team owners 🤓', callback_data = '!owners'},
+			},
+			{
+			{text = 'Team members 👥', callback_data = '!members'},
+			},
+			{
+			{text = 'Our friends 🌹', callback_data = '!friends'},
+			},
+			{
+	    {text = '🔙', callback_data = '!home'},
+	    }
+    }
+    return keyboard
+end
+local function do_keyboard_owners()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+    		{text = '● AmirHo3inF ●', url = 'http://telegram.me/amirho3inf'},},
+			{{text = '● FasT ReaCtoR ●', url = 'http://telegram.me/fastreactor'},},
+			{{text = '● NariMan Master ●', url = 'http://telegram.me/Nawr_i_man'},},
+			{{text = '🔙', callback_data = '!aboutus'},
+	    }
+    }
+    return keyboard
+end
+local function do_keyboard_members()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+			{text = '● Amir SbsS ●' , url = 'telegram.me/amir_h'},
+			{text = '● Mr Jan123 ●', url = 'telegram.me/i_am_jan123'},
+			},
+			{
+			{text = '● MehdiHS ●', url = 'telegram.me/mehdihs'},
+			{text = '● ThisisAmirh ●' , url = 'telegram.me/mr_amirh'},
+			{text = '● M.G.Z ●' , url = 'telegram.me/M_G_Z'},
+			},
+			{
+			{text = '● MrHaliX ●' , url = 'telegram.me/mrhalix'},
+			{text = '● The PoKeR ●', url = 'telegram.me/UnFriendlly'},
+			},
+			{
+			{text = '● AmircC ●', url = 'telegram.me/Amircc_CreeD'},
+			{text = '● PuKeR ●', url = 'telegram.me/pukers'},
+			{text = '● PediW ●', url = 'telegram.me/pediw'},
+			},
+			{
+			{text = '● IDeaCtiVe ●', url = 'telegram.me/Ideactive'},
+			{text = '● YellowHat ●', url = 'telegram.me/yellowhat'},
+			},
+			{
+	    {text = '🔙', callback_data = '!aboutus'},
+        }
+    }
+    return keyboard
+end
+local function do_keyboard_friends()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+	        {
+			{text = '● ShabGarD ●', url = 'telegram.me/night_011'},
+			},
+			{
+			{text = '● Empix ●', url = 'telegram.me/empix'},
+			{text = '● CreeD ●' , url = 'telegram.me/mute_all'},
+			},
+			{
+						{text = '● ReZaHextor ●', url = 'telegram.me/hextor_admin'},
+},
+			{
+	    {text = '🔙', callback_data = '!aboutus'},
+        }
+    }
+    return keyboard
+end
+local action = function(msg,blocks)
+local msg_id = msg.message_id
+local chat = msg.chat.id
+local query = blocks[1]
+    if msg.cb then
+	if query == 'aboutus' then
+		local keyboard = do_keyboard_aboutus()
+		local text = [[_Please select an option ..._]]
+		api.editMessageText(chat, msg_id, text, keyboard, true)
+    end
+	if query == 'owners' then
+		local keyboard = do_keyboard_owners()
+		local text = [[*BeatBotTeam owners 🤓 :*]]
+		api.editMessageText(chat, msg_id, text, keyboard, true)
+    end
+	if query == 'members' then
+		local keyboard = do_keyboard_members()
+		local text = [[*BeatBotTeam admins 👥 :*]]
+		api.editMessageText(chat, msg_id, text, keyboard, true)
+    end
+	if query == 'friends' then
+		local keyboard = do_keyboard_friends()
+		local text = [[*BeatBotTeam friends 🌹 :*]]
+		api.editMessageText(chat, msg_id, text, keyboard, true)
+    end
+	end
+	end
+return {
+  action = action,
+triggers = {
+	    '^###cb:!(aboutus)',
+	    '^###cb:!(owners)',
+	    '^###cb:!(members)',
+	    '^###cb:!(friends)',
+    }
+}
---
+++ plugins/contactus.lua
@@ -0,0 +1,90 @@
+﻿local function do_keyboard_endchat()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+    		{text = '🔚', callback_data = '/end'}
+	    }
+    }
+    return keyboard
+end
+local action = function(msg,blocks, ln)
+local msg_id = msg.message_id
+local user_id = msg.chat.id
+local hash = 'pm:user'
+local chat_info = db:hget(hash,user_id)
+if blocks[1] == 'chat' then
+if chat_info == 'block' then 
+ api.sendMessage(msg.chat.id, '`- Sorry !`\n_You are Blocked_', true) 
+else
+ db:hset(hash, user_id, 'true')
+ api.sendMessage(msg.chat.id, '*- Chat Started*\n `Now all messages will be send to BeatBotTeam Members`\n\n_please send your message now!_', true) 
+ end
+ end
+if blocks[1] == 'end' then
+if chat_info == 'block' or chat_info == 'false' then 
+return nil 
+else
+ db:hset(hash, user_id, 'false')
+api.sendMessage(msg.chat.id, '*- Chat ended* \n/start `Return to home`', true) 
+end
+end
+if msg.chat.type == 'private' and chat_info == 'true' then
+if blocks[1] == 'end' or blocks[1] == 'chat' then return nil end
+api.forwardMessage('-119919729', msg.chat.id, msg_id) 
+api.sendKeyboard(msg.chat.id, '`- Sent !`\n_Please wait for reply_'  ,do_keyboard_endchat(), true)
+end
+if blocks[1] == 'block' then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+msg = msg.reply
+local user_id = msg.forward_from.id
+ db:hset(hash, user_id, 'block')
+api.sendMessage(msg.chat.id, '_User '..user_id..' Blocked_', true) 
+api.sendMessage(user_id, '`- Sorry`\n_You are Blocked_\n\n*Chat ended.*', true) 
+else
+ if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+ if msg.reply then return nil end
+local user_id = blocks[2]
+ db:hset(hash, user_id, 'block')
+api.sendMessage(msg.chat.id, '_User '..user_id..' Blocked_', true) 
+api.sendMessage(user_id, '`- Sorry`\n_You are Blocked_', true) 
+end 
+end
+end
+if blocks[1] == 'unblock' then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+msg = msg.reply
+local user_id = msg.forward_from.id
+ db:hset(hash, user_id, 'false')
+api.sendMessage(msg.chat.id, '_User '..user_id..' UnBlocked_', true) 
+api.sendMessage(user_id, '_You are UnBlocked_', true) 
+else
+ if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+  if msg.reply then return nil end
+local user_id = blocks[2]
+ db:hset(hash, user_id, 'false')
+api.sendMessage(msg.chat.id, '_User '..user_id..' UnBlocked_', true) 
+api.sendMessage(user_id, '_You are UnBlocked_', true) 
+end 
+end
+end
+    if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 then
+   msg = msg.reply_to_message
+    local receiver = msg.forward_from.id
+    local input = blocks[1]
+      api.sendMessage(receiver, '- Reply : \n\n'..input, false)
+end
+end
+return {
+  action = action,
+triggers = {
+    '^/(unblock) (%d+)$',
+    '^/(block) (%d+)$',
+    '^/(unblock)$',
+    '^/(block)$',
+    '^/(chat)$',
+    '^/(end)$',
+	'^###cb:/(chat)',
+	'^###cb:/(end)',
+    '^(.*)$',
+    }
+}
